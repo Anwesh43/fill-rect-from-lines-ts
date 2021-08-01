ce979aeb0f3439c995ee4bca6f89d1e8af5362e9
Anwesh43 <anweshthecool0@gmail.com>
2021-08-01 16:29:48 +0530

    adding FillRectFromLines class

--- index.ts
+++ index.ts
@@ -182,3 +182,26 @@ class FRLNode {
         return this 
     }
 }
+
+class FillRectFromLines {
+
+    curr : FRLNode 
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.curr.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
+    }
+}
